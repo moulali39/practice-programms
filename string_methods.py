d918cc941b9d5f6e9866c5746a5ec2250283e72c
moulali39 <107177595+moulali39@users.noreply.github.com>
2022-07-03 22:44:06 +0530

    Add files via upload

---
+++ string_methods.py
@@ -0,0 +1,62 @@
+# a='my NAME'
+# print(a.capitalize()) # My name
+
+# b='MY WORLD'
+# print(b.casefold()) # my WORLD
+
+c='venkateswararao'
+print(c.center(10,'3')) 
+
+# d='car is indigo,indigo is my car'
+# print(d.count('indigo')) # 2
+
+# e= 'haal moulali'
+# print(e.encode()) # b'hai jaffar'
+
+# f='hallo my dear friends'
+# print(f.endswith('.')) # False
+
+# g='hallo my dear friends my friends are 12'
+# print(g.endswith('12')) # 'true' plase chack the line number_16,17 and 19,20 both are different 
+
+# h='my friend name is vinay'
+# print(h.find('name')) # 9
+
+# i='i like my bike'
+# print(i.index('my')) # 7
+
+# j='moulali150'
+# print(j.isalnum()) # true
+
+# k='moulali'
+# print(k.isalpha()) # true
+
+
+# l='hai'
+# print(l.isascii()) # true
+
+# m='23'
+# print(m.isdecimal()) # true
+
+# m='2.1'
+# print(m.isdecimal()) # false
+
+# n='2356'
+# print(n.isdigit()) # true
+
+# o='mu_nna'
+# print(o.isidentifier()) # true
+
+
+# o='mu.nna'
+# print(o.isidentifier()) # false
+ 
+ 
+
+
+
+
+
+
+
+ 
